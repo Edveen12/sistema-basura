3d94a73e941a3c79654f91fd43e2552d5eef998b
Edveen12 <av1190012024@unab.edu.sv>
2026-05-17 18:05:19 -0600

    Add database folder

---
+++ database/database.js
@@ -0,0 +1,58 @@
+const Database =
+require('better-sqlite3');
+
+const db =
+new Database('basura.db');
+
+
+
+// TABLA USUARIOS
+
+db.prepare(`
+
+CREATE TABLE IF NOT EXISTS usuarios(
+
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+nombre TEXT,
+
+correo TEXT,
+
+password TEXT
+
+)
+
+`).run();
+
+
+
+
+// TABLA REPORTES
+
+db.prepare(`
+
+CREATE TABLE IF NOT EXISTS reportes(
+
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+ubicacion TEXT,
+
+tipo TEXT,
+
+descripcion TEXT,
+
+latitud TEXT,
+
+longitud TEXT,
+
+imagen TEXT,
+
+usuario_id INTEGER
+
+)
+
+`).run();
+
+
+
+module.exports = db;
